2b5e8bd49b8e2a7a634e2817378edfd8ed522070
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2024-04-03 11:47:35 +0100

    current weather section - classes named

--- app/components/currentWeather/CurrentWeather.jsx
+++ app/components/currentWeather/CurrentWeather.jsx
@@ -4,12 +4,12 @@ import Current from "./CurrentWeather.module.css";
 const CurrentWeather = () => {
   return (
     <section className={Current.container}>
-      <div>Date</div>
-      <div>Location</div>
-      <div>Current Temp</div>
-      <div>Feels Like</div>
-      <div>Current Weather</div>
-      <div>High and Low</div>
+      <div className={Current.date}>Date</div>
+      <div className={Current.location}>Location</div>
+      <div className={Current.current_temp}>Current Temp</div>
+      <div className={Current.feels_like}>Feels Like</div>
+      <div className={Current.current_weather}>Current Weather</div>
+      <div className={Current.high_low}>High and Low</div>
     </section>
   );
 };
