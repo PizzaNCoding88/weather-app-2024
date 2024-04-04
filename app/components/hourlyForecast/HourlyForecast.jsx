8d70dfc62aab629b76e12d597a4bd8f90eb398f0
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2024-04-04 21:45:05 +0100

    test

--- app/components/hourlyForecast/HourlyForecast.jsx
+++ app/components/hourlyForecast/HourlyForecast.jsx
@@ -5,41 +5,41 @@ import Image from "next/image";
 const HourlyForecast = () => {
   return (
     <>
-      <div class={Hourly.container}>
+      <div className={Hourly.container}>
         <p>Hourly Forecast</p>
         <div className={Hourly.hourscontainer}>
           <div className={Hourly.hour}>
-            <p class={Hourly.dayhour}>3PM</p>
+            <p className={Hourly.dayhour}>3PM</p>
             <Image alt="weather Icon" src={Sun}></Image>
             <p className={Hourly.temp}>10°C</p>
           </div>
           <div className={Hourly.hour}>
-            <p class={Hourly.dayhour}>3PM</p>
+            <p className={Hourly.dayhour}>3PM</p>
             <Image alt="weather Icon" src={Sun}></Image>
             <p className={Hourly.temp}>10°C</p>
           </div>
           <div className={Hourly.hour}>
-            <p class={Hourly.dayhour}>3PM</p>
+            <p className={Hourly.dayhour}>3PM</p>
             <Image alt="weather Icon" src={Sun}></Image>
             <p className={Hourly.temp}>10°C</p>
           </div>
           <div className={Hourly.hour}>
-            <p class={Hourly.dayhour}>3PM</p>
+            <p className={Hourly.dayhour}>3PM</p>
             <Image alt="weather Icon" src={Sun}></Image>
             <p className={Hourly.temp}>10°C</p>
           </div>
           <div className={Hourly.hour}>
-            <p class={Hourly.dayhour}>3PM</p>
+            <p className={Hourly.dayhour}>3PM</p>
             <Image alt="weather Icon" src={Sun}></Image>
             <p className={Hourly.temp}>10°C</p>
           </div>
           <div className={Hourly.hour}>
-            <p class={Hourly.dayhour}>3PM</p>
+            <p className={Hourly.dayhour}>3PM</p>
             <Image alt="weather Icon" src={Sun}></Image>
             <p className={Hourly.temp}>10°C</p>
           </div>
           <div className={Hourly.hour}>
-            <p class={Hourly.dayhour}>3PM</p>
+            <p className={Hourly.dayhour}>3PM</p>
             <Image alt="weather Icon" src={Sun}></Image>
             <p className={Hourly.temp}>10°C</p>
           </div>
