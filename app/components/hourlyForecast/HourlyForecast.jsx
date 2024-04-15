9b4f070bc1943daf8ffa041f0857f09f0d457d3a
PizzaNCoding88 <110925628+PizzaNCoding88@users.noreply.github.com>
2024-04-15 08:58:03 +0100

    rendering times pt.1

--- app/components/hourlyForecast/HourlyForecast.jsx
+++ app/components/hourlyForecast/HourlyForecast.jsx
@@ -1,3 +1,66 @@
+// import Hourly from "./HourlyForecast.module.css";
+// import Sun from "../../assets/sunny.svg";
+// import Image from "next/image";
+
+// const HourlyForecast = (props) => {
+//   const { data } = props;
+//   console.log(data);
+
+//   function unixToTime(time) {
+//     let date = new Date(time * 1000);
+//     let hours = date.getHours();
+//     return hours;
+//   }
+
+//   console.log(data[0].dt);
+//   return (
+//     <>
+//       <div className={Hourly.container}>
+//         <p>Hourly Forecast</p>
+//         <div className={Hourly.hourscontainer}>
+//           <div className={Hourly.hour}>
+//             <p className={Hourly.dayhour}>{unixToTime(data[0].dt)}:00</p>
+//             <Image alt="weather Icon" src={Sun}></Image>
+//             <p className={Hourly.temp}>10°C</p>
+//           </div>
+//           <div className={Hourly.hour}>
+//             <p className={Hourly.dayhour}>3PM</p>
+//             <Image alt="weather Icon" src={Sun}></Image>
+//             <p className={Hourly.temp}>10°C</p>
+//           </div>
+//           <div className={Hourly.hour}>
+//             <p className={Hourly.dayhour}>3PM</p>
+//             <Image alt="weather Icon" src={Sun}></Image>
+//             <p className={Hourly.temp}>10°C</p>
+//           </div>
+//           <div className={Hourly.hour}>
+//             <p className={Hourly.dayhour}>3PM</p>
+//             <Image alt="weather Icon" src={Sun}></Image>
+//             <p className={Hourly.temp}>10°C</p>
+//           </div>
+//           <div className={Hourly.hour}>
+//             <p className={Hourly.dayhour}>3PM</p>
+//             <Image alt="weather Icon" src={Sun}></Image>
+//             <p className={Hourly.temp}>10°C</p>
+//           </div>
+//           <div className={Hourly.hour}>
+//             <p className={Hourly.dayhour}>3PM</p>
+//             <Image alt="weather Icon" src={Sun}></Image>
+//             <p className={Hourly.temp}>10°C</p>
+//           </div>
+//           <div className={Hourly.hour}>
+//             <p className={Hourly.dayhour}>3PM</p>
+//             <Image alt="weather Icon" src={Sun}></Image>
+//             <p className={Hourly.temp}>10°C</p>
+//           </div>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default HourlyForecast;
+
 import Hourly from "./HourlyForecast.module.css";
 import Sun from "../../assets/sunny.svg";
 import Image from "next/image";
@@ -5,13 +68,25 @@ import Image from "next/image";
 const HourlyForecast = (props) => {
   const { data } = props;
   console.log(data);
+
+  function unixToTime(time) {
+    let date = new Date(time * 1000);
+    let hours = date.getHours();
+    return hours;
+  }
+
+  console.log(data[0].dt);
   return (
     <>
       <div className={Hourly.container}>
         <p>Hourly Forecast</p>
         <div className={Hourly.hourscontainer}>
-          <div className={Hourly.hour}>
-            <p className={Hourly.dayhour}>3PM</p>
+          {data.map((data, key) => {
+            return <div key={key}>{unixToTime(data.dt)}:00</div>;
+          })}
+
+          {/* <div className={Hourly.hour}>
+            <p className={Hourly.dayhour}>{unixToTime(data[0].dt)}:00</p>
             <Image alt="weather Icon" src={Sun}></Image>
             <p className={Hourly.temp}>10°C</p>
           </div>
@@ -44,7 +119,7 @@ const HourlyForecast = (props) => {
             <p className={Hourly.dayhour}>3PM</p>
             <Image alt="weather Icon" src={Sun}></Image>
             <p className={Hourly.temp}>10°C</p>
-          </div>
+          </div> */}
         </div>
       </div>
     </>
